12dc549046f54a7849a0f6489f4836d4bac1e3f5
Mary Elizabeth Lofton <melofton@tc285.cm.cluster>
2024-07-15 11:35:30 -0400

    collating model runs

--- Scripts/wrangle_surrogate_dataset.R
+++ Scripts/wrangle_surrogate_dataset.R
@@ -9,7 +9,7 @@ library(tidyverse)
 library(lubridate)
 
 # rowbind model runs
-runs <- list.files("./Output/", full.names = TRUE)
+runs <- list.files("./Output", full.names = TRUE)
 dat2 <- map_df(runs, read_csv) 
 
 # plot model runs according to value of some parameter
@@ -18,6 +18,8 @@ ggplot(data = dat2, aes(x = datetime, y = prediction, group = R_growth_Nfixer, c
   theme_classic()+
   scale_x_datetime(expand = c(0, 0))+
   scale_y_continuous(expand = c(0, 0))
+ggsave(plot = last_plot(), filename = "./Plots/chla_vs_R_growth_Nfixer.png", device = "png",
+       height = 3, width = 5, units = "in")
 
 # read in observation dataset
 obs <- read_csv("./Data/EXOChla.csv") %>%
@@ -28,6 +30,7 @@ write.csv(obs, file = "./Output/surrogate-observations_12JUL24.csv", row.names =
 
 # write final dataset to file
 final <- dat2 %>%
+  mutate(datetime = date(datetime)) %>%
   filter(datetime %in% obs$DateTime)
 write.csv(final, file = "./Output/surrogate-GLM-AED-runs_12JUL24.csv",row.names = FALSE)
 
